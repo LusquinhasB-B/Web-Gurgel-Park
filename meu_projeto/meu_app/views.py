16b6edb5139c0285a0309fdb8e27d636ddf881a8
LusquinhasB-B <lucassilvasousa2008@gmail.com>
2025-04-26 22:38:35 -0300

    Create home and uptade "Views"

--- meu_projeto/meu_app/views.py
+++ meu_projeto/meu_app/views.py
@@ -4,13 +4,30 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 
-# Create your views here.
-def home(request):
-    return render(request, "home.html")
 
+def home(request):
+    if request.method == "GET":
+        return render(request, "home.html")
+    else:
+        irPrincipal = request.POST.get('irPagina')
+        irLogin = request.POST.get('irLogin')
+        irCadastro = request.POST.get('irCadastro')
+
+        if irPrincipal:
+            return redirect('/auth/pagina/')
+        if irLogin:
+            return redirect('/auth/login/')
+        if irCadastro:
+            return redirect('/auth/cadastro/')
+
+
+
+#----------------------Método Cadastro---------------------#
 
 def cadastro(request):
-    if request.method == "POST":
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
         usuario = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
@@ -22,35 +39,47 @@ def cadastro(request):
         elif not senha:
             return HttpResponse("Senha não inserida")
 
-    # Traz do banco de dados do proprio django se existe um usuairo com o mesmo nome
+        # Se ja existe um usuario com o mesmo nome
         user = User.objects.filter(username=usuario).first()
+
         if user:
             return HttpResponse('Já existe um usuário com esse Username')
         
-    #Pede para criar com outro nome
+        #Pede para criar com outro nome
         user = User.objects.create_user(username=usuario, email=email, password=senha)
         user.save()
 
-        return HttpResponse("Usuário cadastrado com sucesso!")
+        #redireciona o usuario a até a pagina de login
+        return redirect("/auth/login/")
 
-    return render(request, 'cadastro.html')
+#----------------------Método Login---------------------#
+
 def login(request):
-    
     if request.method == "GET":
-        return render(request, 'login.html')
+        return render(request, 'login.html')  
     else:
         usuario = request.POST.get('username')
-        senha = request.POST.get('senha') 
+        senha = request.POST.get('senha')  
 
-        user = authenticate(username=usuario, password=senha)
+        validacao = authenticate(username=usuario, password=senha)
 
-        if user:
-            login_django(request, user)
-            return redirect(request, "pagina.html")
+        if validacao:
+            login_django(request, validacao)
+            return redirect("/auth/pagina/")
         else:
-            return HttpResponse("Usuario ou senhas inválidos")
+            return HttpResponse("<h2>Usuario ou senhas inválidos </h2>"
+            "<br> "
+            "<a href='/auth/login/'>Clique aqui para voltar a area de Login</a>")
         
+
+#----------------------Página Principal---------------------#
+
 def paginaPrincipal(request):
     if request.user.is_authenticated:
         return render(request, 'pagina.html')
-    return HttpResponse("Precisa estar LOGADO PARA acessar a página!")
+    return HttpResponse("Precisa estar LOGADO PARA acessar a página!"
+    "<br>"
+    "<a href='/auth/login/'>Clique aqui para ir a pagina de login </a>" \
+    "<br>" \
+    "<a href='/auth/cadastro/'>Ou clique aqui para ir a pagina de cadastro</a>" \
+    "")
